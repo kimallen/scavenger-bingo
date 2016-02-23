3d8764fc79642004076fe301c6f185e762f2ddfc
kimallen <kimmarieallen@gmail.com>
2016-02-22 16:17:15 -0800

    Migrate db

--- db/migrate/20160223000141_create_rounds.rb
+++ db/migrate/20160223000141_create_rounds.rb
@@ -2,7 +2,7 @@ class CreateRounds < ActiveRecord::Migration
   def change
     create_table :rounds do |t|
     	t.string :url_ext
-    	t.references game
+    	t.references :game
       t.timestamps null: false
     end
   end
