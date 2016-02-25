84dea3092917deffaecb1b4de7bd9cf5c55a88b7
kimallen <kimmarieallen@gmail.com>
2016-02-25 12:27:46 -0800

    Add partial for sign in/sign up for current and not current user

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :users, only: [:show]
 
   # You can have the root of your site routed with "root"
   root 'home#index'
