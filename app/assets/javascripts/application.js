9a900603c09b1781eea64c26f96e077769a44df4
kimallen <kimmarieallen@gmail.com>
2016-03-07 14:29:17 -0800

    Link application.html.erb to application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,19 @@
-
+// ...
 //= require jquery
 //= require jquery_ujs
 
 //= require_tree .
+$(document).ready(function(){
+console.log("document ready")
+clueFormListener()
+
+})
+
+function clueFormListener(){
+	console.log("inside clueFormListener")
+	$("#add_clue").on("submit", "#add_clue_form", function(e){
+		e.preventDefault();
+		console.log("inside preventDefault")
+	})
+}
+
